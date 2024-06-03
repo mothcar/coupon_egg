190a319960b3edb365d8d8cf855add073bdc5302
Paul Hwang <mothcar@gmail.com>
2024-06-04 03:46:53 +0900

    fake data Update

--- src/data/fake.js
+++ src/data/fake.js
@@ -1,17 +1,50 @@
 export const lists = [
   {
-    title: "만리장성 팔아요",
-    imgae : "image1",
+    id: "001",
+    title: "미스터피자 쿠폰 팔아요",
+    imgae : "https://res.cloudinary.com/mothcar/image/upload/v1717440003/coupon/mrpizza.png",
+    content: "왜 파냐면요. 피자 먹고 싶어서 팔아요."
+  },
+  {
+    id: "002",
+    title: "영짜장 쿠폰 팔아요",
+    imgae : "https://res.cloudinary.com/mothcar/image/upload/v1717439990/coupon/yogijjajang.jpg",
     content: "왜 파냐면요. 짜장면이 먹고 싶어서 팔아요."
   },
   {
-    title: "다래성 팔아요",
-    imgae : "image1",
-    content: "왜 파냐면요. 짜장면이 먹고 싶어서 팔아요."
+    id: "003",
+    title: "치킨공주 쿠폰 팔아요",
+    imgae : "https://res.cloudinary.com/mothcar/image/upload/v1717439990/coupon/pizzanara.jpg",
+    content: "왜 파냐면요. 치킨이 먹고 싶어서 팔아요."
   },
   {
+    id: "004",
     title: "홍각정 팔아요",
-    imgae : "image1",
+    imgae : "https://res.cloudinary.com/mothcar/image/upload/v1717439990/coupon/gongjjaru.jpg",
+    content: "왜 파냐면요. 짜장면이 먹고 싶어서 팔아요."
+  },
+  {
+    id: "005",
+    title: "공짜루 쿠폰 팔아요",
+    imgae : "https://res.cloudinary.com/mothcar/image/upload/v1717439990/coupon/gongjjaru.jpg",
+    content: "왜 파냐면요. 짜장면이 먹고 싶어서 팔아요."
+  },
+  {
+    id: "006",
+    title: "평화반점 도장 3개 찍은거 팔아요",
+    imgae : "https://res.cloudinary.com/mothcar/image/upload/v1717439990/coupon/pyunghwa.jpg",
+    content: "왜 파냐면요. 짜장면이 먹고 싶어서 팔아요."
+  },
+  {
+    id: "007",
+    title: "부영궁 쿠폰 팔아요",
+    imgae : "https://res.cloudinary.com/mothcar/image/upload/v1717439990/coupon/buyoungung.jpg",
+    content: "왜 파냐면요. 짜장면이 먹고 싶어서 팔아요."
+  },
+  {
+    id: "008",
+    title: "네네치킨 쿠폰 팔아요",
+    imgae : "https://res.cloudinary.com/mothcar/image/upload/v1717439990/coupon/nene.jpg",
     content: "왜 파냐면요. 짜장면이 먹고 싶어서 팔아요."
   },
 ]
