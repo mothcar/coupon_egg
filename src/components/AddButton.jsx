924cfe2a030a7cdcd04ea5e43af29310df767701
Paul Hwang <mothcar@gmail.com>
2024-06-05 05:37:11 +0900

    등록 + onClick

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -2,12 +2,15 @@ import { BiAdjust } from "react-icons/bi";
 import "./NavBar.scss";
 
 export default function AddButton() {
+  function clickHandler() {
+    console.log("Clicked.........")
+  }
   return (
     <div id="addbutton">
       <button
-        type="button"
         className="inline-block rounded-full bg-sky-400 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
-      >
+        onClick={clickHandler}
+        >
         등록
       </button>
     </div>
