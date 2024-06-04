ac4e37343cd3639d8c9db43da5a802c8dedcfb5a
Paul Hwang <mothcar@gmail.com>
2024-06-05 06:36:24 +0900

    history to replace

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,10 +10,10 @@ export default function NavBar() {
           <h3>신대방2동</h3>
 
           <ul>
-            <li><Link to="/"><span>Home</span> <Icon name="home" /></Link></li>
-            <li><Link to="/about"><span>About</span> <Icon name="about" /></Link></li>
-            <li><Link to="/contact"><span>Contact</span> <Icon name="contact" /></Link></li>
-            <li><Link to="/login"><span>Log in</span> <Icon name="login" /></Link></li>
+            <li><Link to="/" replace><span>Home</span> <Icon name="home" /></Link></li>
+            <li><Link to="/about" replace><span>About</span> <Icon name="about" /></Link></li>
+            <li><Link to="/contact" replace><span>Contact</span> <Icon name="contact" /></Link></li>
+            <li><Link to="/login" replace><span>Log in</span> <Icon name="login" /></Link></li>
           </ul>
         </nav>
       </header>
