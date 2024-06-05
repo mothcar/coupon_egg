2106880a2767ddbb8a21094e06c10023e36d16bc
seulsl <july24thsl@gmail.com>
2024-06-05 17:40:25 +0900

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar"
 import AddButton from "./components/AddButton.jsx"
-import Home from "./pages/Home";
-import About from "./pages/About.jsx";
+import Home from "./pages/Home"
+import About from "./pages/About.jsx"
+import Login from "./pages/Login.jsx"
 
 function App() {
   return (
@@ -16,11 +17,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} exact></Route>
             <Route path="/about" element={<About />} exact></Route>
+            <Route path="/login" element={<Login />} exact></Route>
           </Routes>
         </Router>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,90 @@
+import { React, useRef, useState, useEffect, useContext } from "react"
+import { Route, useNavigate } from "react-router-dom"
+import AuthContext from "../context/AuthProvider"
+import axios from "../api/axios"
+
+function Login() {
+  const { setAuth } = useContext(AuthContext)
+  const LOGIN_URI = "/login"
+  const navigate = useNavigate()
+  const userRef = useRef()
+  const errRef = useRef()
+
+  const [email, setEmail] = useState("")
+  const [pwd, setPwd] = useState("")
+  const [login, setLogin] = useState(false)
+
+  useEffect(() => {
+    userRef.current.focus()
+  }, [])
+
+  //   useEffect(() => {}, [email, pwd])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const configuration = {
+      method: "post",
+      url: "http://localhost:3000/login",
+      data: {
+        email,
+        password: pwd,
+      },
+    }
+    // make the API call
+    axios(configuration)
+      .then((result) => {
+        console.log("result", result)
+        setLogin(true)
+        navigate("/")
+      })
+      .catch((error) => {
+        console.log(error)
+        error = new Error()
+      })
+  }
+
+  return (
+    <>
+      {login ? (
+        alert("login success")
+      ) : (
+        <div>
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input
+                type="text"
+                placeholder="Email"
+                id="email"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+              />
+            </div>
+
+            <div>
+              <p>
+                Don't have an account? <a href="#">Register</a>
+              </p>
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Login
