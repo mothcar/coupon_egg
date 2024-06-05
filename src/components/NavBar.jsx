316d2b16804a5f34e02f663ad46fe93ec12af889
Paul <mothcar@gmail.com>
2024-06-05 17:27:31 +0900

    test2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
     <div id="navbar">
       <header>
         <nav>
-          <h3>황동현2동</h3>
+          <h3>신림2동</h3>
 
           <ul>
             <li><Link to="/" replace><span>Home</span> <Icon name="home" /></Link></li>
