347da533d2a9b70de0da1c1caf4dbd227af04da7
Paul Hwang <mothcar@gmail.com>
2024-06-07 05:58:50 +0900

    detail image

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router-dom";
-import "./common.css"
+import "./common.css";
 
 export default function Detail() {
   const { state } = useLocation();
@@ -8,23 +8,105 @@ export default function Detail() {
     <>
       <div className="common">
         <div>{state.title}</div>
+        <div className="flex justify-center items-center">
+          <img
+            className="h-auto max-w-full"
+            src={state.image}
+            alt="image description"
+          />
+        </div>
         <div>{state.image}</div>
         <div>{state.content}</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam recusandae dolorum voluptas molestias vero voluptatibus illum nisi quidem qui animi quis minima blanditiis quo reprehenderit magni quasi, esse, possimus id?</div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          recusandae dolorum voluptas molestias vero voluptatibus illum nisi
+          quidem qui animi quis minima blanditiis quo reprehenderit magni quasi,
+          esse, possimus id?
+        </div>
       </div>
     </>
   );
