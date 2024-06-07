c4a94a67c84ff1a118b830af2165265e61416f6d
Paul <mothcar@gmail.com>
2024-06-08 01:55:41 +0900

    등록버튼 디자인

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -14,7 +14,7 @@ export default function AddButton() {
   return (
     <div id="addbutton">
       <button
-        className="inline-block rounded-full bg-sky-400 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+        className="inline-block rounded-full bg-blue-500 px-6 pb-2 pt-2.5 text-md font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
         onClick={clickHandler}
       >
         등록
