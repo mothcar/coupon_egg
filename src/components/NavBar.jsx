567a8d5b2188af0b7615201ca3fcdef547ccf97d
Paul <mothcar@gmail.com>
2024-06-09 23:39:40 +0900

    bottom nav : space-around

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,21 @@
-import { Link } from 'react-router-dom';
-import './NavBar.scss';
-import Icon from './Icon';
+import { Link } from "react-router-dom";
+import "./NavBar.scss";
+import Icon from "./Icon";
 import { BsPerson } from "react-icons/bs";
 
 export default function NavBar() {
   return (
     <div id="navbar">
       <header>
-        <nav>
+        <nav className="items-center">
           <h3>신림2동</h3>
 
-          <ul className='items-center'>
+          <ul className="">
             <li><Link to="/home" replace><span>Home</span> <Icon name="home" /></Link></li>
-            {/* <li><Link to="/about" replace><span>About</span> <Icon name="about" /></Link></li>
-            <li><Link to="/contact" replace><span>Contact</span> <Icon name="contact" /></Link></li> */}
             <li><Link to="/login" replace><span>Log in</span> <BsPerson style={{fontSize: 25}} /></Link></li>
           </ul>
         </nav>
       </header>
     </div>
-  )
+  );
 }
