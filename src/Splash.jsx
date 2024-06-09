adde32538ea00d43ccd16f35df04fab9f4374fa1
Paul Hwang <mothcar@gmail.com>
2024-06-10 05:33:09 +0900

    ignore : margin

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -31,9 +31,7 @@ export default function Splash() {
       <h6 className="mt-5">세상의 모든 쿠폰 거래</h6>
       <img src={pandaLogo} alt="" className="mt-5"/>
       <h6 className="mt-5 text-white">스마트폰 url address bar 때문에 글을 위로 올려야하기때문에 </h6>      
-      <h6 className="mt-5 text-white">...</h6>      
-      <h6 className="mt-5 text-white">...</h6>      
-      <h6 className="mt-5 text-white">...</h6>      
+      <h6 className="mt-5 text-white">...</h6>  
     </div>
   )
 }
