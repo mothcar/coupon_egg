c8bb3ff68824b1f098aeafba9c010b99683f58f0
Paul Hwang <mothcar@gmail.com>
2024-06-10 05:26:31 +0900

    panda Logo

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom";
+import pandaLogo from "./assets/red_panda.jpg";
 
 
 export default function Splash() {
@@ -27,7 +28,8 @@ export default function Splash() {
   return (
     <div className="flex flex-col m-auto h-screen justify-center items-center ">
       <h1 className="text-3xl font-medium">쿠폰나라</h1>
-      <h6 className="mt-5">세상의 모든 쿠폰 거래</h6>      
+      <h6 className="mt-5">세상의 모든 쿠폰 거래</h6>
+      <img src={pandaLogo} alt="" />
       <h6 className="mt-5 text-white">스마트폰 url address bar 때문에 글을 위로 올려야하기때문에 </h6>      
       <h6 className="mt-5 text-white">...</h6>      
       <h6 className="mt-5 text-white">...</h6>      
