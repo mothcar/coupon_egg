093395334fd2c365021e3d756ef31fb776294b83
Paul <mothcar@gmail.com>
2024-06-11 17:12:47 +0900

    거주지역찾기-우편번호 검색으로

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useRef, useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 // import AuthContext from "../context/AuthProvider";
 import axios from "../api/axios";
+import DaumPostcode from 'react-daum-postcode';
 
 function Login() {
   // const { setAuth } = useContext(AuthContext);
@@ -49,6 +50,54 @@ function Login() {
     navigate("../", { replace: true })
   }
 
+  // 주소찾기 ********************************************
+  // const [detailedAddress, setDetailedAddress] = useState('');
+
+  const [zonecode, setZonecode] = useState('');
+  const [address, setAddress] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const themeObj = {
+    bgColor: '#FFFFFF', 
+    pageBgColor: '#FFFFFF', 
+    postcodeTextColor: '#C05850',
+    emphTextColor: '#222222',
+  };
+
+  const postCodeStyle = {
+    width: '360px',
+    height: '480px',
+  };
+
+  const completeHandler = (data) => {
+    console.log('Complete address : ', data.sido, " 구군 : ", data.sigungu, " 동 : ", data.bname    )
+    const { address, zonecode } = data;
+    setZonecode(zonecode);
+    setAddress(address);
+  };
+
+  const closeHandler = (state) => {
+    if (state === 'FORCE_CLOSE') {
+      setIsOpen(false);
+    } else if (state === 'COMPLETE_CLOSE') {
+      setIsOpen(false);
+    }
+  };
+
+  const toggleHandler = () => {
+    setIsOpen((prevOpenState) => !prevOpenState);
+  };
+
+  const inputChangeHandler = (event) => {
+    console.log('Final : ', event.target.value)
+    // setDetailedAddress(event.target.value);
+  };
+
+  function setValue() {
+    console.log('Address : ', address)
+  }
+
+
   return (
     <>
       <div className="pt-5 pb-16">
@@ -91,6 +140,38 @@ function Login() {
             <button onClick={logoutHandler}>Log out</button>
           </div>
         )}
+      </div>
+
+      <div className="mb-10">
+        <label className="block text-base mt-4 mb-2">거주지역찾기</label>
+        <input
+          type="text"
+          id="address"
+          onClick={toggleHandler}
+          onChange={setValue}
+          value={address}
+          className="border w-full h-12 text-base px-2 py-1 focus:outline-none focus:right-1 focus:border-gray-600 rounded-md"
+          placeholder="거주지역찾기"
+        />
+      </div>
+
+      <div id="address">
+      {isOpen && (
+          <div className="mb-12">
+            <DaumPostcode
+              theme={themeObj}
+              style={postCodeStyle}
+              onComplete={completeHandler}
+              onClose={closeHandler}
+            />
+          </div>
+        )}
+        <div>{zonecode}</div>
+        <div>{address}</div>
+        <input
+          // value={detailedAddress}
+          onChange={inputChangeHandler}
+        />
       </div>
     </>
   );
