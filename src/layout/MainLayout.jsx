41eb151d163bb8f8dab60fe250e33c0576c138a3
Paul <mothcar@gmail.com>
2024-06-11 15:54:33 +0900

    등록버튼 Home에서만

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,10 @@
 import NavBar from "../components/NavBar.jsx";
-import AddButton from "../components/AddButton.jsx";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
     <>
       <NavBar />
-      <AddButton />
       <Outlet />
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,18 @@
-import {lists} from "../data/fake.js"
-import Item from "../components/Item.jsx"
-
+import { lists } from "../data/fake.js";
+import Item from "../components/Item.jsx";
+import AddButton from "../components/AddButton.jsx";
 
 export default function Home() {
   // console.log('Get data : ', lists)
   return (
     <>
       <div className="pt-2 pb-16 divide-y divide-solid divide-gray-200">
-        {
-          lists.map(list=>(
-            // <Card className="" key={list.id} id={lists.id} title={list.title} image={list.image} content={list.content} />
-            <Item className="" key={list.id} card={list} />
-          ))
-        }
+        {lists.map((list) => (
+          // <Card className="" key={list.id} id={lists.id} title={list.title} image={list.image} content={list.content} />
+          <Item className="" key={list.id} card={list} />
+        ))}
       </div>
+      <AddButton />
     </>
-  )
+  );
 }
