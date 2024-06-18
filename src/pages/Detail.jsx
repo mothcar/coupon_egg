3625a80f492088d56eced084343e5fc378da3977
Paul <mothcar@gmail.com>
2024-06-19 00:24:50 +0900

    1. Detail : image margin 2. Home : remove test get

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -27,52 +27,55 @@ export default function Detail() {
 
   return (
     <>
-      <div className="common">
-        <div className="flex justify-center items-center">
+      <div>
+        <div className="mt-10">
           <img
-            className="h-auto max-w-full"
+            className="h-80 w-full object-cover"
             src={state.coupon_image}
             alt="image description"
           />
         </div>
-        <div className="mt-2 flex items-center gap-3">
-          <div>
-            {state.userImage ? (
-              <img src={state.userImage} alt="" />
-            ) : (
-              <BsPerson fontSize={30} />
-            )}
+        <div className="common">
+        <div className="flex items-center gap-3">
+            <div>
+              {state.userImage ? (
+                <img src={state.userImage} alt="" />
+              ) : (
+                <BsPerson fontSize={30} />
+              )}
+            </div>
+            <div>
+              <div className="text-lg font-bold">{state.userName}</div>
+              <dir className="text-sm font-thin">{state.addressDepth3}</dir>
+            </div>
           </div>
-          <div>
-            <div className="text-lg font-bold">{state.userName}</div>
-            <dir className="text-sm font-thin">{state.addressDepth3}</dir>
+          <div className="mt-5 text-xl font-bold">{state.title}</div>
+          <div className="text-sm text-gray-600">{state.shopName}</div>
+          <div className="mt-5 min-h-20 text-base">{state.content}</div>
+          <div className="mt-5 flex justify-between">
+            <div className="text-base font-bold">거래희망장소</div>
+            <div>{state.place}</div>
           </div>
-        </div>
-        <div className="mt-5 text-xl font-bold">{state.title}</div>
-        <div className="text-sm text-gray-600">{state.shopName}</div>
-        <div className="mt-5 min-h-20 text-base">{state.content}</div>
-        <div className="mt-5 flex justify-between">
-          <div className="text-base font-bold">거래희망장소</div>
-          <div>{state.place}</div>
-        </div>
-        <Map
-          center={{ lat: 33.5563, lng: 126.79581 }}
-          style={{ width: widthNew, height: heightNew }}
-          className="mb-14 rounded-lg"
-        >
-          <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-            <div style={{ color: "#000" }}>{state.place}</div>
-          </MapMarker>
-        </Map>
-        <div className="fixed inset-x-0 bottom-0 text-center z-10">
-          <button
-            type="button"
-            onClick={tradeHandler}
-            className=" w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-5 rounded"
+          <Map
+            center={{ lat: 33.5563, lng: 126.79581 }}
+            style={{ width: widthNew, height: heightNew }}
+            className="mb-14 rounded-lg"
           >
-            교환제안,제안하기
-          </button>
+            <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+              <div style={{ color: "#000" }}>{state.place}</div>
+            </MapMarker>
+          </Map>
+          <div className="fixed inset-x-0 bottom-0 text-center z-10">
+            <button
+              type="button"
+              onClick={tradeHandler}
+              className=" w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-5 rounded"
+            >
+              교환제안,제안하기
+            </button>
+          </div>
         </div>
+        
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { lists } from "../data/fake.js";
+// import { lists } from "../data/fake.js";
 import Item from "../components/Item.jsx";
 import AddButton from "../components/AddButton.jsx";
 import { useEffect, useState } from "react";
@@ -19,8 +19,8 @@ export default function Home() {
   },[])
   return (
     <>
-      <div className="mt-10 text-lg font-bold">Get List :  {coupons.length}</div>
-      <div className="pt-2 pb-16 divide-y divide-solid divide-gray-200">
+      {/* <div className="mt-10 text-lg font-bold">Get List :  {coupons.length}</div> */}
+      <div className="pb-16 divide-y divide-solid divide-gray-200">
         {coupons.map((list) => (
           // <Card className="" key={list.id} id={lists.id} title={list.title} image={list.image} content={list.content} />
           <Item className="" key={list._id} card={list} />
